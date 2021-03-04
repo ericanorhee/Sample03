eab119064bb0406c74641685f8cca1fbf6c3cc95
Ericano Rhee <ericano.rhee@gmail.com>
2021-03-05 05:17:36 +0900

    Initial Commit

---
+++ Shared/Sample03App.swift
@@ -0,0 +1,17 @@
+//
+//  Sample03App.swift
+//  Shared
+//
+//  Created by 40번 맥 on 2021/03/05.
+//
+
+import SwiftUI
+
+@main
+struct Sample03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
